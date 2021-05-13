1e4a7a96132603e3357870cbebf039ccafc0b917
andrey_fedorov <andrjuxa97@mail.ru>
2021-05-13 12:36:49 +0400

    add not image component and admin fast edit from post component, configure map service

---
+++ laravel/resources/js/frontend/components/not-images/NotImageYetComponent.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="not-image-container ">
+        <span style='font-size:100px;'>&#128533;</span>
+        <h2>Здесь ещё нет изображений</h2>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "NotImageYetComponent"
+    }
+</script>
+
+<style scoped>
+
+    .not-image-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
